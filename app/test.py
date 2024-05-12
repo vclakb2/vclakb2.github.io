891400007fc811560f89566402da0ab7d5e684af
Chili404 <kchilukuri@idslabs.org>
2024-05-12 18:34:58 -0400

    fix connector

--- app/test.py
+++ app/test.py
@@ -2,25 +2,23 @@ import flet as ft
 import mysql.connector
 
 def main(page: ft.Page):
-    myConnection = mysql.connector.connect( 
-        user = 'avnadmin',
-        password = 'AVNS_978XTtRvLUWrowzEW-D',
-        host = 'mysql-3b07a8e5-db-developer.f.aivencloud.com',
-        post = '13447',
-        database = 'DevAI'
-    )
+    myConnection = mysql.connector.connect(user = 'avnadmin',
+                                       password = 'AVNS_978XTtRvLUWrowzEW-D',
+                                       host = 'mysql-3b07a8e5-db-developer.f.aivencloud.com',
+                                       port = 13447,
+                                       database = 'DevAI')
     
     def add_clicked(e):
         cursor = myConnection.cursor()
-        results = []
 
         query1 = """
         SELECT * FROM Developer
         """
         cursor.execute(query1)
-        results.append(cursor.fetchall())
+        txt = cursor.fetchone()
+        txt = f"{txt}"
         page.add(ft.Checkbox(label=new_task.value))
-        new_task.value = results[0]
+        new_task.value = txt
         page.update()
 
     new_task = ft.TextField(hint_text="What's needs to be done?")
