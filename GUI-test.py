90291c3065b52e87038d8a656ba717ea0ec3304d
plopor <bowen.luo@gmail.com>
2024-05-12 17:27:26 -0500

    removed password again

--- GUI-test.py
+++ GUI-test.py
@@ -4,7 +4,6 @@ import csv
 import re
 
 myConnection = mysql.connector.connect(user = 'avnadmin',
-                                       password = '',
                                        host = 'mysql-3b07a8e5-db-developer.f.aivencloud.com',
                                        port = 13447,
                                        database = 'DevAI')
