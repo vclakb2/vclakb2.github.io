84dbb8265a31bc9e1bb718e9c88c60c8632a7409
plopor <bowen.luo@gmail.com>
2024-05-12 17:21:09 -0500

    simplegui test

---
+++ GUI-test.py
@@ -0,0 +1,78 @@
+import PySimpleGUI as sg
+import mysql.connector
+import csv
+import re
+
+myConnection = mysql.connector.connect(user = 'avnadmin',
+                                       password = 'AVNS_978XTtRvLUWrowzEW-D',
+                                       host = 'mysql-3b07a8e5-db-developer.f.aivencloud.com',
+                                       port = 13447,
+                                       database = 'DevAI')
+
+cursorObject = myConnection.cursor()
+
+def executeAndPrint(query):
+    cursorObject.execute(query)
+    allResults = cursorObject.fetchall()
+
+    layout = [[sg.Table(allResults, justification='left', key='-TABLE-')],]
+    window = sg.Window("Title", layout, finalize=True)
+
+    while True:
+        event, values = window.read()
+        if event == sg.WINDOW_CLOSED:
+            break
+        print(event, values)
+
+    window.close()
+
+def constructVariableQuery():
+    variableLayout = [  
+                        [sg.Text("What DevID?")],
+                        [sg.InputText()],
+                        [sg.Button('OK'), sg.Button('Cancel')] 
+                     ]
+
+    window = sg.Window('ExampleVariable', variableLayout)
+
+    while True:
+        event, values = window.read()
+
+        if event == sg.WIN_CLOSED or event == 'Cancel':
+            break
+
+        if event == 'OK':
+            executeAndPrint(f'SELECT * FROM Developer WHERE DevID={values[0]}')
+
+    window.close()
+
+query_1 = \
+        """
+        SELECT * FROM Developer
+        """
+
+query_2 = \
+        """
+        SELECT * FROM Country
+        """
+
+layout = [  
+            [sg.Button('Query1'), sg.Button('Query2'), sg.Button('Query3')] 
+         ]
+
+window = sg.Window('Example', layout)
+
+while True:
+    event, values = window.read()
+
+    if event == sg.WIN_CLOSED:
+        break
+
+    if event == 'Query1':
+        executeAndPrint(query_1)
+    elif event == 'Query2':
+        executeAndPrint(query_2)
+    elif event == 'Query3':
+        constructVariableQuery()
+
+window.close()
